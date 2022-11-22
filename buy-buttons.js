7d9ecab173165535b76c0e8dc54d836649e5dc52
gabrielbrasil03 <118781124+gabrielbrasil03@users.noreply.github.com>
2022-11-21 21:38:49 -0300

    Create buy-buttons.js

---
+++ buy-buttons.js
@@ -0,0 +1,214 @@
+/*<![CDATA[*/
+//Goethe-Zertifikat A1 - Fit in Deutsch 1
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'icba-blumenau.myshopify.com',
+      storefrontAccessToken: 'f593c3d36e6e62c507a7f9b4b5ad9d7b',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '8251692908775',
+        node: document.getElementById('product-component-1668782826493'),
+        moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D',
+        options: buttonCss,
+      });
+    });
+  }
+})();
+/*]]>*/
+
+//Goethe-Zertifikat A1 - Start in Deutsch 1
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'icba-blumenau.myshopify.com',
+      storefrontAccessToken: 'f593c3d36e6e62c507a7f9b4b5ad9d7b',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '8251693269223',
+        node: document.getElementById('product-component-1669043395504'),
+        moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D',
+        options: buttonCss,
+      });
+    });
+  }
+})();
+
+//Goethe-Zertifikat A2
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'icba-blumenau.myshopify.com',
+      storefrontAccessToken: 'f593c3d36e6e62c507a7f9b4b5ad9d7b',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '8251694645479',
+        node: document.getElementById('product-component-1669074186320'),
+        moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D',
+        options: buttonCss,
+      });
+    });
+  }
+})();
+
+//Goethe-Zertifikat A2 - Start in Deutsch 2
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'icba-blumenau.myshopify.com',
+      storefrontAccessToken: 'f593c3d36e6e62c507a7f9b4b5ad9d7b',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '8251694219495',
+        node: document.getElementById('product-component-1669074577414'),
+        moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D',
+        options: buttonCss,
+      });
+    });
+  }
+})();
+
+//Goethe-Zertifikat B1
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'icba-blumenau.myshopify.com',
+      storefrontAccessToken: 'f593c3d36e6e62c507a7f9b4b5ad9d7b',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '8252862464231',
+        node: document.getElementById('product-component-1669074778614'),
+        moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D',
+        options: buttonCss,
+      });
+    });
+  }
+})();
+
+//Goethe-Zertifikat B1
+/*<![CDATA[*/
+(function () {
+  var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = scriptURL;
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient({
+      domain: 'icba-blumenau.myshopify.com',
+      storefrontAccessToken: 'f593c3d36e6e62c507a7f9b4b5ad9d7b',
+    });
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent('product', {
+        id: '8252871049447',
+        node: document.getElementById('product-component-1669075848187'),
+        moneyFormat: 'R%24%20%7B%7Bamount_with_comma_separator%7D%7D',
+        options:  buttonCss,
+      });
+    });
